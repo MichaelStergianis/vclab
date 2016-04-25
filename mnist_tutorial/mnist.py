f51f289064b1bfa808db79f50147d04acc9ab563
Michael Stergianis <michaelstergianis@gmail.com>
2016-04-25 14:13:39 -0400

    MNIST: Training code implemented from tutorial, evaluation next

--- mnist_tutorial/mnist.py
+++ mnist_tutorial/mnist.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import numpy as np
 import cv2
 
@@ -13,5 +14,33 @@ images = mnist.train.images
 ###     cv2.imshow('image', np.reshape(i, (28, 28), order='C'))
 ###     cv2.waitKey(0)
 ###     cv2.destroyAllWindows()
+
+# initialize our variables and placeholders
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+# softmax on W*x + b
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+# training step
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+# initialize our variables
+init = tf.initialize_all_variables()
+
+# create a session and run
+session = tf.Session()
+session.run(init)
+
+# run the training step 1000 times
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    session.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 
