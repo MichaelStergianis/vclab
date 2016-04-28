12a3015e7072912890f3b59f2cdaf20250d6c335
Michael Stergianis <MichaelStergianis@gmail.com>
2016-04-28 14:04:29 -0400

    Summing Network: Added python file to randomly generate test and train data

---
+++ sum_network/sum_gen.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+import numpy
+import random
+import time
+
+random.seed(time.clock())
+
+# create training data
+with open("ran_nums_train.csv", "w") as f:
+    for i in range(55000):
+        sum = 0
+        for i in range(4):
+            temp = random.randint(0, 255)
+            sum += temp
+            s = str(temp)
+            f.write(s)
+            f.write(",")
+        s = str(sum)
+        f.write(s)
+        f.write("\n")
+
+# create testing data
+with open("ran_nums_test.csv", "w") as f:
+    for i in range(10000):
+        sum = 0
+        for i in range(4):
+            temp = random.randint(0, 255)
+            sum += temp
+            s = str(temp)
+            f.write(s)
+            f.write(",")
+        s = str(sum)
+        f.write(s)
+        f.write("\n")
+
