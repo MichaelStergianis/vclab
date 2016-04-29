6d42597dbca1fa830e1a9203f93c0e5d4dd4656b
Michael Stergianis <michaelstergianis@gmail.com>
2016-04-29 15:42:13 -0400

    Sum Network: Small update to reflect interface changes

--- sum_network/sum_network.py
+++ sum_network/sum_network.py
@@ -2,18 +2,10 @@
 
 import tensorflow as tf
 import numpy
-from data_type import DataType as dt
+from DataImport import *
 
-# if no data exists, let's create it for now
-# will probably delete this later for more flexibility
-if not os.path.isdir("./data"):
-    os.system("sum_gen.py")
+# import the randomly generated training and testing data
+training_data = TrainData()
+test_data = TestData()
 
-# read in the training data
-training_data_list = []
-with open("data/ran_nums_train.csv") as f:
-    while line = f.readline():
-        line = line.split(",")
-	training_data_list.append(dt([line[0], line[1], line[2], line[3]], line[4]))
 
-print(training_data_list[1])
