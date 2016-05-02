5c01a6afd0c775a585164fba599232f239bb8ce6
Michael Stergianis <michaelstergianis@gmail.com>
2016-05-02 15:01:14 -0400

    Sum Network: sum_network implemented with naive mnist strategy

--- sum_network/DataImport.py
+++ sum_network/DataImport.py
@@ -14,9 +14,12 @@ class DataImport:
             for line in f:
                 line = line[:-1].split(",")
                 assert len(line) == 5
-                self.arrays.append([[int(line[0]), int(line[1])], 
-                                    [int(line[2]), int(line[3])]])
-                self.labels.append(int(line[4]))
+                self.arrays.append([int(line[0]), int(line[1]), int(line[2]), int(line[3])])
+                label = []
+                for i in range(1024):
+                    label.append(0)
+                label[ (int(line[4]) - 1) ] = 1
+                self.labels.append(label)
 
     def next_batch(self, size):
         # randomly select size numbers to provide
@@ -26,7 +29,7 @@ class DataImport:
         listSize = len(self.arrays)
         assert size <= listSize
         for i in range(size):
-            index = random.randint(0, listSize)
+            index = random.randint(0, listSize-1)
             arrayRetList.append(self.arrays[index])
             labelsRetList.append(self.labels[index])
         return (arrayRetList, labelsRetList)
@@ -35,11 +38,11 @@ class DataImport:
         assert len(self.arrays) == len(self.labels)
         for i in range(len(self.arrays)):
             sum = 0
-            for j in range(2):
-                for k in range(2):
-                    sum += self.arrays[i][j][k]
-            if sum != self.labels[i]:
-                print("Array does not match sum")
+            for j in range(4):
+                sum += self.arrays[i][j]
+            if self.labels[i][sum - 1] != 1:
+                print("Array does not match label")
+
 
     def printIndex(self, index):
         print("Array = [["  + str(self.arrays[index][0][0]) + ", " + str(self.arrays[index][0][1])
@@ -61,9 +64,9 @@ class TestData:
     def __init__(self):
         if not os.path.isdir("data"):
             DataGenerator.main(55000, 10000)
-        d = DataImport()
-        d.parse("data/ran_nums_test.csv")
-        d.verify()
+        self.d = DataImport()
+        self.d.parse("data/ran_nums_test.csv")
+        self.d.verify()
 
     def next_batch(self, size):
         return self.d.next_batch(size)
--- sum_network/sum_network.py
+++ sum_network/sum_network.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python2
 
 import tensorflow as tf
 import numpy
@@ -8,4 +8,31 @@ from DataImport import *
 training_data = TrainData()
 test_data = TestData()
 
+# first attempt will be just a softmax layer
 
+x = tf.placeholder(tf.float32, [None, 4])
+
+W = tf.Variable(tf.ones([4, 1024]))
+b = tf.Variable(tf.zeros([1024]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 1024])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(.01).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+for i in range(5000):
+    batch_x, batch_y = training_data.next_batch(5000)
+    train_accuracy = sess.run(accuracy, feed_dict={x:batch_x, y_:batch_y})
+    print("step %d, training accuracy %g"%(i, train_accuracy))
+    sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
+
+print(sess.run(accuracy, feed_dict={x: test_data.d.arrays, y_: test_data.d.labels}))
