26949dd6f15045febfb6c40687b705d90ac2c731
Michael Stergianis <MichaelStergianis@gmail.com>
2016-05-03 12:37:00 -0400

    Sum Network: modified DataImport to include inheritence as well as bitwise lists

--- sum_network/DataImport.py
+++ sum_network/DataImport.py
@@ -9,17 +9,54 @@ class DataImport:
         self.arrays = []
         self.labels = []
 
-    def parse(self, file):
+    def parse_whole(self, file):
         with open(file, "r") as f:
             for line in f:
                 line = line[:-1].split(",")
                 assert len(line) == 5
-                self.arrays.append([int(line[0]), int(line[1]), int(line[2]), int(line[3])])
+                num_list = []
+                for i in line[:-1]:
+                    num_list.append(int(i))
+                self.arrays.append(num_list)
                 label = []
                 for i in range(1024):
                     label.append(0)
-                label[ (int(line[4]) - 1) ] = 1
+                label[ (int(line[-1]) - 1) ] = 1
                 self.labels.append(label)
+
+    """
+    _bitwise
+
+    Returns a list of the binary values of the number starting with the least
+    significant bit. For now it is only useable for values up to 255, but that
+    is intentional given our dataset
+
+    number -- the number to be put into bitwise fashion
+    """
+    def _bitwise(self, number):
+        ret_list = []
+        for i in range(0, 8):
+            temp = number & 2**i
+            temp = temp >>i
+            ret_list.append(temp)
+        return ret_list
+
+    
+    def parse_bitwise(self, file):
+        with open(file, "r") as f:
+            for line in f:
+                line = line[:-1].split(",")
+                assert len(line) == 5
+                num_list = []
+                for i in line[:-1]:
+                    num_list.append(self._bitwise(int(i)))
+                self.arrays.append(num_list)
+                label = []
+                for i in range(1021):
+                    label.append(0)
+                label[ (int(line[-1]) - 1) ] = 1
+                self.labels.append(label)
+
 
     def next_batch(self, size):
         # randomly select size numbers to provide
@@ -38,7 +75,7 @@ class DataImport:
         assert len(self.arrays) == len(self.labels)
         for i in range(len(self.arrays)):
             sum = 0
-            for j in range(4):
+            for j in range(len(self.arrays[0])):
                 sum += self.arrays[i][j]
             if self.labels[i][sum - 1] != 1:
                 print("Array does not match label")
@@ -49,24 +86,18 @@ class DataImport:
             + "\n         " + str(self.arrays[index][1][0]) + ", " + str(self.arrays[index][1][1])
             + "]]" + "\nsum = " + str(self.labels[index]))
 
-class TrainData:
-    def __init__(self):
-        if not os.path.isdir("data"):
-            DataGenerator.main(55000, 10000)
-        self.d = DataImport()
-        self.d.parse("data/ran_nums_train.csv")
-        self.d.verify()
+class TrainData(DataImport):
+    def __init__(self, bitwise=False):
+        DataImport.__init__(self)
+        if bitwise:
+            self.parse_bitwise("data/ran_nums_train.csv")
+        else:
+            self.parse_whole("data/ran_nums_train.csv")
 
-    def next_batch(self, size):
-        return self.d.next_batch(size)
-
-class TestData:
-    def __init__(self):
-        if not os.path.isdir("data"):
-            DataGenerator.main(55000, 10000)
-        self.d = DataImport()
-        self.d.parse("data/ran_nums_test.csv")
-        self.d.verify()
-
-    def next_batch(self, size):
-        return self.d.next_batch(size)
+class TestData(DataImport):
+    def __init__(self, bitwise=False):
+        DataImport.__init__(self)
+        if bitwise:
+            self.parse_bitwise("data/ran_nums_test.csv")
+        else:
+            self.parse_whole("data/ran_nums_test.csv")
