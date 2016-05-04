f46a53681ebfbb3e5dca9a60053a6a1a67060135
Michael Stergianis <MichaelStergianis@gmail.com>
2016-05-04 09:54:44 -0400

    Sum Network: updated to reflect changes in import

--- sum_network/sum_network.py
+++ sum_network/sum_network.py
@@ -15,7 +15,7 @@ def bias_variable(shape):
 
 # function definitions for code cleanliness
 def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME', use_cudnn_on_gpu=True)
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,1,1,1], padding='SAME')
@@ -24,18 +24,19 @@ def max_pool_2x2(x):
 training_data = TrainData(bitwise=True)
 test_data = TestData(bitwise=True)
 
-x = tf.placeholder(tf.float32, shape=[None, 40], name="Placeholder_x")
+x = tf.placeholder(tf.float32, shape=[None, 32], name="Placeholder_x")
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 ####################### FIRST LAYER ###############################
 ####################################################################
 
 # initialize our variables
-W_conv1 = weight_variable([5, 2, 1, 32])
+W_conv1 = weight_variable([3, 3, 1, 32])
 b_conv1 = bias_variable([32])
 
 # reshape x to a 4-d tensor
-x_mat = tf.reshape(x, [-1, 10, 4, 1])
+x_mat = tf.reshape(x, [-1, 4, 8, 1])
+print(x_mat)
 
 h_conv1 = tf.nn.relu(conv2d(x_mat, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
@@ -46,7 +47,7 @@ h_pool1 = max_pool_2x2(h_conv1)
 ####################### SECOND LAYER ###############################
 ####################################################################
 
-W_conv2 = weight_variable([5, 2, 32, 64])
+W_conv2 = weight_variable([3, 3, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -57,7 +58,7 @@ h_pool2 = max_pool_2x2(h_conv2)
 ####################### THIRD LAYER ################################
 ####################################################################
 
-W_conv3 = weight_variable([5, 2, 64, 128])
+W_conv3 = weight_variable([3, 3, 64, 128])
 b_conv3 = bias_variable([128])
 
 h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
@@ -68,10 +69,10 @@ h_pool3 = max_pool_2x2(h_conv3)
 ####################### DENSE LAYER ################################
 ####################################################################
 
-W_fc1 = weight_variable([4 * 10 * 128, 1024])
+W_fc1 = weight_variable([4 * 8 * 128, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool3_flat = tf.reshape(h_pool3, [-1, 4*10*128])
+h_pool3_flat = tf.reshape(h_pool3, [-1, 4*8*128])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
 
 # Dropout layer
@@ -101,7 +102,7 @@ sess = tf.Session()
 sess.run(tf.initialize_all_variables())
 
 for i in range(10001):
-    batch_x, batch_y = training_data.next_batch(250)
+    batch_x, batch_y = training_data.next_batch(750)
     if i %100 == 0:
         train_accuracy = sess.run(accuracy, feed_dict={x:batch_x, y_:batch_y, keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
