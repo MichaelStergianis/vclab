52e9b2fdbdaa370d1a2b50a0f2d9b5c39768f47b
Michael Stergianis <MichaelStergianis@gmail.com>
2016-05-12 13:28:46 -0400

    Added support to select gpu tasks in mnist

--- mnist_tutorial/mnist.py
+++ mnist_tutorial/mnist.py
@@ -37,38 +37,41 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 ######################## FIRST LAYER #############################
 ##################################################################
 
-# initialize our variables
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-
-# reshape x to 4d tensor
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-# convolution and max pool
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+with tf.device("/gpu:1"):
+    # initialize our variables
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    
+    # reshape x to 4d tensor
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+    
+    # convolution and max pool
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
 ##################################################################
 
 ######################## SECOND LAYER ############################
 ##################################################################
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+with tf.device("/gpu:0"):
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
 ##################################################################
 
 ######################## DENSE LAYER #############################
 ##################################################################
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+with tf.device("/gpu:1"):
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 ##################################################################
 
